a0531621659177396a8719b5508b94c674713082
YukiWakabayashi0922 <ee193143@meiji.ac.jp>
2023-02-16 18:33:01 +0900

    sample_file first_challenge

--- include/first_challenge_sample/first_challenge_sample.h
+++ include/first_challenge_sample/first_challenge_sample.h
@@ -3,9 +3,8 @@
 
 #include <ros/ros.h>
 #include <roomba_500driver_meiji/RoombaCtrl.h>
-// #include <nav_msgs/Odometry.h>
-// #include <sensor_msgs/LaserScan.h>
-// #include <tf/tf.h>
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/LaserScan.h>
 
 class FirstChallenge
 {
@@ -14,28 +13,21 @@ class FirstChallenge
         void process();
 
     private:
-        // void odometry_callback(const nav_msgs::Odometry::ConstPtr&);
-        // void laser_callback(const sensor_msgs::LaserScan::ConstPtr&);
-        // float Getyaw();
-        // float Getrange_min();
+        void odometry_callback(const nav_msgs::Odometry::ConstPtr&);
+        void laser_callback(const sensor_msgs::LaserScan::ConstPtr&);
 
-        // void run();
-        void turn();
-        // void stop();
-        // void show_odom();
-        // void show_yaw();
-        // void show_scan();
+        void run();
 
         int hz_;
 
-        // nav_msgs::Odometry odometry_;
-        // sensor_msgs::LaserScan laser_;
+        nav_msgs::Odometry odometry_;
+        sensor_msgs::LaserScan laser_;
         roomba_500driver_meiji::RoombaCtrl cmd_vel_;
 
         ros::NodeHandle nh_;
         ros::NodeHandle private_nh_;
-        // ros::Subscriber sub_odom_;
-        // ros::Subscriber sub_laser_;
+        ros::Subscriber sub_odom_;
+        ros::Subscriber sub_laser_;
         ros::Publisher pub_cmd_vel_;
 
 };
--- src/first_challenge_sample.cpp
+++ src/first_challenge_sample.cpp
@@ -3,12 +3,12 @@
 FirstChallenge::FirstChallenge():private_nh_("~")
 {
     private_nh_.param("hz", hz_, {10});
-    // sub_odom_ = nh_.subscribe("/roomba/odometry", 100, &FirstChallenge::odometry_callback, this);
-    // sub_laser_ = nh_.subscribe("/scan", 100, &FirstChallenge::laser_callback, this);
+    sub_odom_ = nh_.subscribe("/roomba/odometry", 100, &FirstChallenge::odometry_callback, this);
+    sub_laser_ = nh_.subscribe("/scan", 100, &FirstChallenge::laser_callback, this);
     pub_cmd_vel_ = nh_.advertise<roomba_500driver_meiji::RoombaCtrl>("/roomba/control", 1);
 }
 
-/*void FirstChallenge::odometry_callback(const nav_msgs::Odometry::ConstPtr& msg)
+void FirstChallenge::odometry_callback(const nav_msgs::Odometry::ConstPtr& msg)
 {
     odometry_ = *msg;
 }
@@ -18,77 +18,21 @@ void FirstChallenge::laser_callback(const sensor_msgs::LaserScan::ConstPtr& msg)
     laser_ = *msg;
 }
 
-float FirstChallenge::Getyaw()
-{
-    double roll, pitch, yaw;
-    tf::Quaternion quat(odometry_.pose.pose.orientation.x, odometry_.pose.pose.orientation.y, odometry_.pose.pose.orientation.z, odometry_.pose.pose.orientation.w);
-    tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);
-
-    return float(yaw);
-}
-
-float FirstChallenge::Getrange_min()
-{
-    float range_min = 1e6;
-    int size_medium = laser_.ranges.size()/2;
-    int size_min = size_medium - 20;
-    int size_max = size_medium + 20;
-    for(int i=size_min; i<size_max; i++) {
-        if(laser_.ranges[i] < range_min) {
-            range_min = laser_.ranges[i];
-        }
-    }
-    return float(range_min);
-}
-
 void FirstChallenge::run()
 {
     cmd_vel_.mode = 11;
-    cmd_vel_.cntl.linear.x = 0.1;
-    cmd_vel_.cntl.angular.z = 0.0;
-
-    pub_cmd_vel_.publish(cmd_vel_);
-}*/
-
-void FirstChallenge::turn()
-{
-    cmd_vel_.mode = 11;
     cmd_vel_.cntl.linear.x = 0.2;
-    cmd_vel_.cntl.angular.z = M_PI/8;
+    cmd_vel_.cntl.angular.z = M_PI/6;
 
     pub_cmd_vel_.publish(cmd_vel_);
 }
-
-/*void FirstChallenge::stop()
-{
-    cmd_vel_.mode = 11;
-    cmd_vel_.cntl.linear.x = 0.0;
-    cmd_vel_.cntl.angular.z = 0.0;
-
-    pub_cmd_vel_.publish(cmd_vel_);
-}
-
-void FirstChallenge::show_odom()
-{
-    std::cout << "odom" << ": x:" << odometry_.pose.pose.position.x << " y:" << odometry_.pose.pose.position.y << " z:" << odometry_.pose.pose.position.z << std::endl;
-}
-
-void FirstChallenge::show_yaw()
-{
-    std::cout << "yaw:" << Getyaw() << std::endl;
-}
-
-void FirstChallenge::show_scan()
-{
-    std::cout << "scan: min:" << Getrange_min() << std::endl;
-}*/
 
 void FirstChallenge::process()
 {
     ros::Rate loop_late(hz_);
     while(ros::ok())
     {
-        turn();
+        run();
         ros::spinOnce();
         loop_late.sleep();
     }
@@ -99,6 +43,5 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "first_challenge_sample");
     FirstChallenge first_challenge;
     first_challenge.process();
-    ros::spin();
     return 0;
 }
