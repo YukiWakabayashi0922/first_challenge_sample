d917960beef3a2b98ae24ec85dc9034691db5972
YukiWakabayashi0922 <ee193143@meiji.ac.jp>
2023-02-16 17:47:13 +0900

    test

---
+++ include/first_challenge_sample/first_challenge_sample.h
@@ -0,0 +1,43 @@
+#ifndef FIRST_CHALLENGE_SAMPLE_H
+#define FIRST_CHALLENGE_SAMPLE_H
+
+#include <ros/ros.h>
+#include <roomba_500driver_meiji/RoombaCtrl.h>
+// #include <nav_msgs/Odometry.h>
+// #include <sensor_msgs/LaserScan.h>
+// #include <tf/tf.h>
+
+class FirstChallenge
+{
+    public:
+        FirstChallenge();
+        void process();
+
+    private:
+        // void odometry_callback(const nav_msgs::Odometry::ConstPtr&);
+        // void laser_callback(const sensor_msgs::LaserScan::ConstPtr&);
+        // float Getyaw();
+        // float Getrange_min();
+
+        // void run();
+        void turn();
+        // void stop();
+        // void show_odom();
+        // void show_yaw();
+        // void show_scan();
+
+        int hz_;
+
+        // nav_msgs::Odometry odometry_;
+        // sensor_msgs::LaserScan laser_;
+        roomba_500driver_meiji::RoombaCtrl cmd_vel_;
+
+        ros::NodeHandle nh_;
+        ros::NodeHandle private_nh_;
+        // ros::Subscriber sub_odom_;
+        // ros::Subscriber sub_laser_;
+        ros::Publisher pub_cmd_vel_;
+
+};
+
+#endif
